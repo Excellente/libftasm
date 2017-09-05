7dfbf4bfb1e7b60e6053bc87c551ef784caefa7f
Excellent MSIMANGO <emsimang@e4r7p1.wtc.co.za>
2017-09-05 17:07:54 +0200

    removed update

--- print.s
+++ print.s
@@ -6,14 +6,20 @@ _print:
     mov rcx, rdi            ; argument from main
     mov r8,  rsi            ; argument from main
 
+    ; SET-UP
+    push    rbp
+    mov     rbp, rsp
+    sub     rsp, 8
+
     ; CODE
-    mov rdi, 1              ; sys_call arg1
-    mov rsi, rcx            ; sys_call arg2
-    mov rdx, r8             ; sys_call arg3
-    mov rax, 0x2000004      ; sys_call opcode
+    mov     rdi, 1              ; sys_call arg1
+    mov     rsi, rcx            ; sys_call arg2
+    mov     rdx, r8             ; sys_call arg3
+    mov     rax, 0x2000004      ; sys_call opcode
     syscall
 
     ; EXIT
+    pop rbp
     mov rax, 0x2000001
     mov rdx, 0
     syscall
